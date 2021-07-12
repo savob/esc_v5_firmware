2da8b7e4652e1784395da20b3456628d2649afec
Savo Bajic <bajic.savo@gmail.com>
2021-07-12 12:01:50 -0400

    Copied code from previous Arduino sketch

---
+++ lib/motor.h
@@ -0,0 +1,72 @@
+#include <Arduino.h>
+
+extern volatile byte sequenceStep; // Stores step in spinning sequence
+
+extern volatile bool motorUpslope; // Used for PWM
+extern volatile bool test2;
+
+// PWM variables
+extern volatile byte period;           // MUST be less than 254
+extern volatile byte duty;
+extern volatile byte nDuty;  // Used to store the "inverse" duty, to reduce repeated operations withinm the interupt to calculate this
+extern byte endClampThreshold;           // Stores how close you need to be to either extreme before the PWM duty is clamped to that extreme
+
+// AH_BL, AH_ CL, BH_CL, BH_AL, CH_AL, CH_BL
+extern volatile byte motorPortSteps[6];
+
+// Other variables
+extern volatile byte cyclesPerRotation;
+extern volatile byte cycleCount;
+
+extern volatile unsigned int currentRPM;
+extern volatile unsigned int targetRPM;
+extern volatile byte controlScheme;
+
+extern bool reverse;
+extern volatile bool motorStatus; // Stores if the motor is disabled (false) or not
+
+extern const unsigned int spinUpStartPeriod;
+extern const unsigned int spinUpEndPeriod;
+extern const byte stepsPerIncrement;
+extern const unsigned int spinUpPeriodDecrement;
+
+// Buzzer period limits
+extern const unsigned int maxBuzzPeriod;
+extern const unsigned int minBuzzPeriod;
+
+
+
+////////////////////////////////////////////////////////////
+// Function declarations
+
+
+void setPWMDuty(byte temp);
+
+bool enableMotor(byte startDuty);
+void windUpMotor();
+void disableMotor();
+
+void buzz(int periodMicros, int durationMillis);
+
+void AH_BL();
+void AH_CL();
+void BH_CL();
+void BH_AL();
+void CH_AL();
+void CH_BL();
+
+/* Since we are comparing to relative the positive terminal, our edges have to be reversed
+
+  E.g. If A is rising it will go from being less than the zero to more.
+  Since A is connected to the negative terminal, the comparator output will go from 1 to 0
+
+  So although we are looking for a rising edge, our microcontroller will see a falling one
+
+  NOTE THAT ALL THESE WILL ENABLE THE COMPARATOR INTERRUPT!
+*/
+void BEMF_A_RISING();
+void BEMF_A_FALLING();
+void BEMF_B_RISING();
+void BEMF_B_FALLING();
+void BEMF_C_RISING();
+void BEMF_C_FALLING();
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,81 @@
 #include <Arduino.h>
+#include <avr/interrupt.h>
+#include "i2c.h"
+#include "motor.h"
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200); // Debugging/testing
+
+  // Input / config pins
+  DDRD  &= 0x03;  // Configure all input pins as inputs
+  PORTD &= 0x03;  // Clear all input pullups
+  PORTD |= 0xB8;  // Pull ups for all input solder pads (soldered ones short to GND)
+
+  delay(1); // Allow outputs to settle before reading
+
+  // Sets reverse to true if reverse pin is shorted
+  reverse = ((PIND & 0x80) == 0);
+
+  // Read I2C address settings
+  byte temp = PIND & 0x38;  // Extract the 3 bits for setting
+  temp ^= 0x38;             // Flips the bits (since I am shorting the pads I want to set as "1"
+  temp = temp >> 3;         // Shift so bits are in spots 0 to 2 inclusive (0 to 7 decimal value)
+  i2cAddress += temp;       // Add offest to default value
+
+  Wire.begin(i2cAddress);
+  Wire.onRequest(i2cRequest);
+  Wire.onReceive(i2cRecieve);
+  //  Serial.print("I2C Address of ESC is ");
+  //  Serial.println(i2cAddress);
+
+  // Disable pull-up resistors
+  MCUCR |= (1 << PUD);
+  // We don't pull-ups at any other point, so it's best
+  // disabled pins stay hi-Z regardless of PORTx values
+
+  // Analog comparator setting
+  ADCSRA = (0 << ADEN);     // Disable the ADC module
+  ADCSRB = (1 << ACME);     // Enable MUX select for negative input of comparator
+  ACSR   = (0 << ACBG);     // Select PD6 as positive side for comparison
+
+  disableMotor(); // Ensure motor is disabled at start
+
+  Serial.println(F("START"));
 }
 
+
+
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  if (test2) { // Debugging printer
+    Serial.println(F("xxx"));
+    test2 = false;
+    //Serial.println(sequenceStep);
+
+    delay(5);
+  }
+  
+  if (Serial.available()) {
+    Serial.print(F("Current motor status: "));
+    Serial.println(motorStatus);
+    Serial.print(F("Current duty value is "));
+    Serial.println(duty);
+    
+    int temp = Serial.parseInt();
+    temp = constrain(temp, 0 , period);
+    Serial.print(F("Input PWM was read as "));
+    Serial.println(temp);
+    
+    enableMotor(temp);
+
+    Serial.print(F("Resulting motor status: "));
+    Serial.println(motorStatus);
+    Serial.print(F("Resulting duty value is "));
+    Serial.println(duty);
+    Serial.println();
+    
+    delay(10000); // Run for a short period for now
+  }
+
+  disableMotor();
 }
