60a23e8c19b989b2d2839104abb0adb308fbe39c
Savo Bajic <bajic.savo@gmail.com>
2021-07-13 21:02:42 -0400

    Include motor commands now

--- lib/i2c/i2c.h
+++ lib/i2c/i2c.h
@@ -1,4 +1,5 @@
 #include <Wire.h>
+#include "motor.h"
 
 extern byte i2cAddress; // I2C address. Starts with a default, then adds offset according to soldering pads
 extern byte currentInstruction; // Current instruction recieved by ESC from controller
