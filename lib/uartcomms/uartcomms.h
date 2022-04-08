36cc3ac8f182bba94a681a5217a9789e24e87fa2
Savo Bajic <bajic.savo@gmail.com>
2022-04-08 15:38:47 -0400

    Not running with UART for now

--- lib/uartcomms/uartcomms.h
+++ lib/uartcomms/uartcomms.h
@@ -1,7 +1,7 @@
 #ifndef ESC_UARTCOMMS_HEADER
 #define ESC_UARTCOMMS_HEADER
 
-#define ALLOW_UART_COMMS  // Are we enabling UART?
+//#define ALLOW_UART_COMMS  // Are we enabling UART?
 #ifdef ALLOW_UART_COMMS
 #define UART_COMMS_DEBUG  // Do we want debug statements over UART? (Cannot be defined with UART being enabled)
 #endif
